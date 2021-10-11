a5bbcdf7c8d119f1293d61a8f82912b9ed0f63dd
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-10-11 18:27:47 +0300

    -add new struct to hold task -chang variable name's and class name -add enum for Priority -func for delet Item,add Item,print To DO List

--- ToDoList.playground/Contents.swift
+++ ToDoList.playground/Contents.swift
@@ -1,64 +1,45 @@
 import UIKit
 
-class ToDoList{
+enum Priority {
+    case High
+    case medium
+    case Low
+}
+struct Task{
     var id : Int
     var title : String
     var date : String
     var time : String
-    enum priority : String  {
-        case High
-        case medium
-        case Low
-    }
-    
-    init(id : Int , title : String , date : String , time : String , priority : String ){
-        self.id = id
-        self.title = title
-        self.date = date
-        self.time = time
-    }
+    var priority : Priority
+
 }
-class ReminderList : ToDoList{
-    var list : [String]
-    init (list : [String]){
-    self.list = list
-        super.init(id: 1010, title: "To Do List", date: "11 October", time: "12:00", priority: "Low")
-    }
-    func creatList (newList : [String]){
-        let list : [String] = newList
-        print("your new List is : \(list)")
-    }
+class Operation {
+    var theList : [Task]
     
-    func addItem (newItem : String){
-        list.append(newItem)
+    init(theList : Task) {
+        self.theList = [theList]
+    }
+    func addItem (newItem : Task) {
+        theList.append(newItem)
         print("Successfully added")
-        
     }
     
     func deletItem (removeItem : Int){
-        if (removeItem > list.count || removeItem < 0){
+        if (removeItem > theList.count || removeItem < 0){
             print("there is no item in this place!!")
         }else{
-         list.remove(at: removeItem)
+         theList.remove(at: removeItem)
             print("Successfully removed")
         }
     }
-    func printInfo(){
-        print("your id \(id) the title of list is: \(title) the date and time: \(date) at \(time) the priority \(priority.Low) \n your to do list information is : \(list)")
-    }
     func printList(){
-        print("your to do list information is : \(list)")
+        print("your to do list information is : \(theList)")
     }
 }
 
-var mylist = ReminderList(list: ["call mom" , "go to superMarket"])
-mylist.printInfo()
-mylist.addItem(newItem: "do home work")
+var mylist = Operation(theList: Task.init(id: 09, title: "do project", date: "11 October", time: "9:00am", priority: .High))
+mylist.addItem(newItem: Task.init(id: 08, title: "go to gym", date: "11 Octiber", time: "1:00am", priority: .Low))
 mylist.printList()
 mylist.deletItem(removeItem: 0)
 mylist.printList()
-var mylist2 = ReminderList(list: ["go gym" , "visit aunt "])
-mylist.addItem(newItem: "finish project")
-mylist.printList()
-mylist.deletItem(removeItem: 9)
-mylist.printList()
+
