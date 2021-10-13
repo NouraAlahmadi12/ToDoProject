1fb6ac9a64bf225430a404a7e01ee1fa84adca6f
Noura.alahmadi.med.s@tuwaiq.edu.sa <Teeko1122>
2021-10-13 18:00:15 +0300

    - add new enum ( status , Task Error) - add new struct to creat multiple ToDoList - add new func : - delet all List - edit Task - list All Item - get task - order by complete - order by pending - add func in class to print name list , number of task , tasks

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # ToDoProject
+
+##List your Task..
+
+##About my project (Overview)
+
+### it's program that allow you to list your task and print it 
+
+## Usage
+- Add new task -  func addItem (newItem : Task)
+- Edit existing task - editTask (newTask: String, newDate :String , newTime:String , newPriority: Priority)
+- Delete existing task --------> deletItem (byId : Int)
+- Get specific task by Id ---->  getTask(byId: Int) 
+- Order By Complete  --------->  orderByComplete()
+- Order By Pending ----------->  orderByPending () 
+- Print all your tasks ------->  readAllLists()
+- create New List ------------>  createNewList(list: List)
+
+## favorite functions work
+- DeleteTask(byId: Int) : Take Id and check if there task with this id delete it, other ways Throw error
+
+## Technic
+
+### declering 3 enums for : 
+- Priority ( high , medium , Low)
+- Status ( complete , pending)
+- TaskError ( Item Not Found , Invalid Value)
+### Use Throws to handle some error
+
+## Future iterations
+- Sort Lists by date
+- Filter Items by date or completed status
--- ToDoList.playground/Contents.swift
+++ ToDoList.playground/Contents.swift
@@ -1,45 +1,135 @@
 import UIKit
+import Darwin
 
 enum Priority {
     case High
     case medium
     case Low
 }
-struct Task{
+
+enum Status {
+    case complete
+    case pending
+}
+
+enum TaskError : Error {
+    
+    case ItemNotFound
+    case InvalidValue
+    
+}
+
+struct Task {
     var id : Int
-    var title : String
+    var task : String
     var date : String
     var time : String
-    var priority : Priority
-
-}
-class Operation {
-    var theList : [Task]
+    var priority: Priority
+    var status : Status
     
-    init(theList : Task) {
-        self.theList = [theList]
+    mutating func editTask (newTask: String, newDate :String , newTime:String , newPriority: Priority){
+        self.task = newTask
+        self.date = newDate
+        self.time = newTime
+        self.priority = newPriority
     }
-    func addItem (newItem : Task) {
-        theList.append(newItem)
+}
+struct List {
+    var title: String
+    var items: [Task] = []
+    
+    mutating func addItem (newItem : Task) {
+        self.items.append(newItem)
         print("Successfully added")
     }
+    mutating func deletItem (byId : Int) throws {
+        guard let deletTask = self.items.firstIndex(where: {$0.id == byId})
+        else {
+            throw TaskError.ItemNotFound
+        }
+        self.items.remove(at: deletTask)
+        print("Successfully removed")
+    }
+    mutating func deleteAll(){
+        self.items.removeAll()
+        print("Successfully removed")
+    }
     
-    func deletItem (removeItem : Int){
-        if (removeItem > theList.count || removeItem < 0){
-            print("there is no item in this place!!")
-        }else{
-         theList.remove(at: removeItem)
-            print("Successfully removed")
+    mutating func getTask(byId: Int) throws {
+        guard let task = self.items.firstIndex(where: {$0.id == byId})
+        else {
+            throw TaskError.InvalidValue
+        }
+        print(task)
+    }
+    
+    mutating func orderByComplete(){
+        for item in self.items {
+            if (item.status == .complete){
+                print("\(item.task) - \(item.status)")
+            }
         }
     }
-    func printList(){
-        print("your to do list information is : \(theList)")
+    mutating func orderByPending (){
+        for item in self.items {
+            if (item.status == .pending){
+                print("\(item.task) - \(item.status)")
+            }
+        }
     }
 }
 
-var mylist = Operation(theList: Task.init(id: 09, title: "do project", date: "11 October", time: "9:00am", priority: .High))
-mylist.addItem(newItem: Task.init(id: 08, title: "go to gym", date: "11 Octiber", time: "1:00am", priority: .Low))
-mylist.printList()
-mylist.deletItem(removeItem: 0)
-mylist.printList()
+class Operation {
+    var lists : [List] = []
+    
+    func readAllLists(){
+        for list in self.lists {
+            print("List: \(list.title) (\(list.items.count))")
+            for item in list.items {
+                print("Id task : \(item.id) - task :  \(item.task) - date : \(item.date) - time: \(item.time) - the priority is: \(item.priority) - status: \(item.status)")
+            }
+        }
+    }
+    func createNewList(list: List){
+        
+        self.lists.append(list)
+        print(" added")
+    }
+    
+}
+var myLists = Operation()
+var task1 = Task.init(id: 1, task: "go to abha", date: "18 October", time: "7:43am", priority: .High, status: .complete)
+var task2 = Task.init(id: 2, task: "go to jeddah", date: "23 October", time: "9:35am", priority: .medium, status: .complete)
+var task3 = Task.init(id: 3, task: "return to medinah", date: "27 October", time: "10:00am", priority: .High, status: .pending)
+var travelList = List(title: "travel List", items: [task1 , task2 , task3])
+travelList.addItem(newItem: Task.init(id: 4, task: "go to tabuck", date: "25 October", time: "1:00pm", priority: .medium, status: .pending))
+travelList.orderByComplete()
+travelList.orderByPending()
+do {
+    try travelList.deletItem(byId: 3)
+} catch TaskError.ItemNotFound {
+    print ("Item not found")
+} catch TaskError.InvalidValue {
+    print ("Invalid Valud")
+} catch {
+    print ("Unknown Error")
+}
 
+do {
+    try travelList.getTask(byId: 643)
+    
+} catch TaskError.ItemNotFound {
+    print ("Item not found")
+} catch TaskError.InvalidValue {
+    print ("Invalid Valud")
+} catch {
+    print ("Unknown Error")
+}
+myLists.createNewList(list: travelList)
+myLists.readAllLists()
+var homework1 = Task.init(id: 1, task: "to do project", date: "14 October", time: "9:00am", priority: .High, status: .complete)
+var homeWork2 = Task.init(id: 35, task: "presentation", date: "14 October", time: "12:00pm", priority: .High, status: .pending)
+homework1.editTask(newTask: "presentation", newDate: "14 October", newTime: "9:00am", newPriority: .medium)
+var homeWorkList = List(title: "home Work List", items: [homework1 , homeWork2])
+myLists.createNewList(list: homeWorkList)
+myLists.readAllLists()
